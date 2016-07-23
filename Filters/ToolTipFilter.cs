747690d294f7baa6dfba1588ea99ffcbd70c7987
Omid Nasri <omid.nasri@outlook.com>
2016-07-24 00:08:54 +0430

    First Initial.

---
+++ Filters/ToolTipFilter.cs
@@ -0,0 +1,29 @@
+﻿using Orchard.Mvc.Filters;
+using System.Web.Mvc;
+using Orchard.UI.Admin;
+using Orchard.UI.Resources;
+
+namespace Orchard.ToolTip.Filters
+{
+    public class ToolTipFilter : FilterProvider, IResultFilter
+    {
+        private readonly IResourceManager _resourceManager;
+
+        public ToolTipFilter(IResourceManager resourceManager)
+        {
+            _resourceManager = resourceManager;
+        }
+
+        public void OnResultExecuted(ResultExecutedContext filterContext) { }
+
+        public void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            if (AdminFilter.IsApplied(filterContext.RequestContext)) return;
+
+            if (!(filterContext.Result is ViewResult)) return;
+            
+            _resourceManager.RegisterFootScript("<script src=\"/modules/orchard.tooltip/scripts/tooltip.js\"></script>");
+            _resourceManager.RegisterLink(new LinkEntry() { Href = "/modules/orchard.tooltip/styles/tooltip.css", Rel = "stylesheet", Type = "text/css" });
+        }
+    }
+}
